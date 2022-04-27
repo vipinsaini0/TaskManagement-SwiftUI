fd0bdb967466164c56338e6472caade72bf29dca
Vipin Saini <vipinsaini0@users.noreply.github.com>
2022-04-27 22:16:20 +0530

    Initial Commit

---
+++ TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by Vipin Saini on 27/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
